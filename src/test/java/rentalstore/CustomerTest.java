f76c6d82aa1db797387e2da0f35b6cfdcb248067
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-08-19 14:10:39 +0800

    add NEW_RELEASE_statement_rent_three_days test

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -9,7 +9,7 @@ public class CustomerTest {
     @Test
     public void should_return_REGULAR_statement_rent_two_days() {
         Customer customer = new Customer("test");
-        Rental rental = new Rental(new Movie("title1", 0), 2);
+        Rental rental = new Rental(new Movie("title1", Movie.REGULAR), 2);
         customer.addRental(rental);
 
         String statement = customer.statement();
@@ -23,7 +23,7 @@ public class CustomerTest {
     @Test
     public void should_return_REGULAR_statement_rent_three_days() {
         Customer customer = new Customer("test");
-        Rental rental = new Rental(new Movie("title1", 0), 3);
+        Rental rental = new Rental(new Movie("title1", Movie.REGULAR), 3);
         customer.addRental(rental);
 
         String statement = customer.statement();
@@ -33,5 +33,20 @@ public class CustomerTest {
                 "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter points", statement);
     }
+
+    @Test
+    public void should_return_NEW_RELEASE_statement_rent_three_days() {
+        Customer customer = new Customer("test");
+        Rental rental = new Rental(new Movie("title1", Movie.NEW_RELEASE), 3);
+        customer.addRental(rental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for test\n" +
+                "\ttitle1\t9.0\n" +
+                "Amount owed is 9.0\n" +
+                "You earned 2 frequent renter points", statement);
+    }
+
 
 }
