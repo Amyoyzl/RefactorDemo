dc6f8e4f627f260e5b27e72fc43d835a397941d2
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-08-19 19:40:39 +0800

    refactor: extract isNotAgedBrieAndNotBackstage

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -8,8 +8,7 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (isNotAgedBrieAndNotBackstage(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -57,5 +56,10 @@ public class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isNotAgedBrieAndNotBackstage(Item item) {
+        return !item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
