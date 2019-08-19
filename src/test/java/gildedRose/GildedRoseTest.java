0eab1448687091db22f562b7ba42624226ccd4ce
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-08-19 18:58:11 +0800

    add test should_update_gildedRose_items_when_updateQuality_given_item_name_Backstage_and_sellIn_less_than_zero_and_quality_3

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -39,4 +39,15 @@ public class GildedRoseTest {
         assertEquals(updateItem.toString(), items[0].toString());
     }
 
+    @Test
+    public void should_update_gildedRose_items_when_updateQuality_given_item_name_Backstage_and_sellIn_less_than_zero_and_quality_3() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -2, 3)};
+        GildedRose gildedRose = new GildedRose(items);
+        Item updateItem =  new Item("Backstage passes to a TAFKAL80ETC concert", -3, 0);
+
+        gildedRose.updateQuality();
+
+        assertEquals(updateItem.toString(), items[0].toString());
+    }
+
 }
