4abbd5a3ea5f982d915b2533f2d1f8ac09337018
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-08-19 14:02:15 +0800

    add REGULAR_statement_rent_three_days test

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -14,10 +14,24 @@ public class CustomerTest {
 
         String statement = customer.statement();
 
-        assertEquals(statement, "Rental Record for test\n" +
+        assertEquals("Rental Record for test\n" +
                 "\ttitle1\t2.0\n" +
                 "Amount owed is 2.0\n" +
-                "You earned 1 frequent renter points");
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_REGULAR_statement_rent_three_days() {
+        Customer customer = new Customer("test");
+        Rental rental = new Rental(new Movie("title1", 0), 3);
+        customer.addRental(rental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for test\n" +
+                "\ttitle1\t3.5\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter points", statement);
     }
 
 }
