de305c53eedc618d27d560c53008dc9ef66bf34a
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-08-19 18:39:59 +0800

    add test should_update_gildedRose_items_when_updateQuality_given_item_name_Aged_Brie_and_sellIn_less_than_zero_and_quality_50

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -17,4 +17,17 @@ public class GildedRoseTest {
         assertEquals(updateItem.toString(), items[0].toString());
     }
 
+    @Test
+    public void should_update_gildedRose_items_when_updateQuality_given_item_name_Aged_Brie_and_sellIn_less_than_zero_and_quality_50() {
+        Item[] items = new Item[]{new Item("Aged Brie", -2, 50)};
+        GildedRose gildedRose = new GildedRose(items);
+        Item updateItem =  new Item("Aged Brie", -3, 50);
+
+        gildedRose.updateQuality();
+
+        assertEquals(updateItem.toString(), items[0].toString());
+    }
+
+
+
 }
